fe84510b8ea7fccfc3936d322aec65788eb626b6
imamhossain94 <imamagun94@gmail.com>
2021-10-17 22:20:10 +0600

    feat: checking a datetime is gather then current time or not

--- Checking a datetime is gather then current time or nor.dart
+++ Checking a datetime is gather then current time or nor.dart
@@ -19,13 +19,12 @@ import 'package:intl/intl.dart';
 void main() {
   DateTime startDateTime = DateFormat("dd-MM-yyyy HH:mm").parse("17-10-2021 22:00");
   DateTime nowDateTime = DateTime.now();
-  
   int duration = nowDateTime.difference(startDateTime).inSeconds;
-
+  
   print(startDateTime);
   print(nowDateTime);
   print(duration);
-
+  
   if(duration <= 0){
     print("completed");
   }else{
